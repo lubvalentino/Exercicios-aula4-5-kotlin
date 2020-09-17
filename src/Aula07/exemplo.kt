bc8ba6eab8cb2cfccea87c4a89af43907c07394c
lubvalentino <lubvalentino@yahoo.com.br>
2020-09-17 14:34:43 -0300

    Exercicios aula 07

--- src/Aula07/exemplo.kt
+++ src/Aula07/exemplo.kt
@@ -4,18 +4,20 @@ fun main(){
     var itemvenda2 = ItemVenda ("blusa", 5,10.0)
     var itemServico1 = Servico ("consertar",3,6.0)
     var itemServico2 = Servico ("lavar", 4,12.0)
-    var registro = RegistroRecebimentos ()
+    val registro = RegistroRecebimentos ()
 
     println(registro.adicionarRecebimento(itemServico1))
-    println(registro.adicionarRecebimento(itemvenda1))
     println(registro.adicionarRecebimento(itemServico2))
+    println(registro.adicionarRecebimento(itemvenda1))
     println(registro.adicionarRecebimento(itemvenda2))
-    registro.apresentarRecebimento()
+    println(registro.apresentarRecebimento())
 
 }
 
 interface IRecebivel {
     fun totalizarReceita () : Double
+
+    fun tooString():String
 }
 
 class ItemVenda (val produto: String, val quantidade: Int, val valor: Double) : IRecebivel {
@@ -26,33 +28,53 @@ class ItemVenda (val produto: String, val quantidade: Int, val valor: Double) : 
         return valorVenda
     }
 
-    fun tooString () : String{
+    override fun tooString(): String {
         return "A compra de ${quantidade} unidades de ${produto} no valor de R$ ${valor} foi concluída, resultando em R$ ${valorVenda}"
     }
-}
+
+    }
 class Servico (val descricao : String, val horas: Int, val precoHora: Double) : IRecebivel {
     var valorServico = 0.0
     override fun totalizarReceita(): Double {
         valorServico = horas*precoHora
         return valorServico
     }
-    fun tooString () : String {
-       return "O serviço ${descricao} realizado em ${horas} horas por R$ ${precoHora}/hora foi concluído, resultando em R$ ${valorServico}"
+
+    override fun tooString(): String {
+        return "O serviço ${descricao} realizado em ${horas} horas por R$ ${precoHora}/hora foi concluído, resultando em R$ ${valorServico}"
     }
+
+
 }
 
 class RegistroRecebimentos {
     var total = 0.0
-    fun adicionarRecebimento (item: IRecebivel) {
-        total+=item.totalizarReceita()
+    val listaDeProdutos: MutableList<IRecebivel> = mutableListOf()
 
+    fun registroRecebimentos (){
+        total = 0.0
+        listaDeProdutos.forEach{
+            total += it.totalizarReceita()
+
+//            when {
+//                it is ItemVenda -> println(it.tooString())
+//                it is Servico -> println(it.tooString())
+//            }
+        }
+
+    }
+    fun adicionarRecebimento (item: IRecebivel) {
+        listaDeProdutos.add(item)
+        registroRecebimentos()
         when {
             item is ItemVenda -> println(item.tooString())
             item is Servico -> println(item.tooString())
         }
     }
+
     fun apresentarRecebimento(){
+        registroRecebimentos()
         println("Valor total de recebimentos é R$ ${total}")
+
     }
 }
-
